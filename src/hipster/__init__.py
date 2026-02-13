f4bfcf9ff570881b9d9c99b02452586e7d58bb7b
Bernd Doser <bernd.doser@h-its.org>
2026-02-13 14:32:17 +0100

    add CSVGenerator

--- src/hipster/__init__.py
+++ src/hipster/__init__.py
@@ -1,6 +1,7 @@
 import importlib.metadata
 
 from .absorption_line_plotter import AbsorptionLinePlotter
+from .csv_generator import CSVGenerator
 from .hips_generator import HiPSGenerator
 from .html_generator import HTMLGenerator
 from .image_generator import ImageGenerator
@@ -14,6 +15,7 @@ from .votable_generator import VOTableGenerator
 __version__ = importlib.metadata.version("astro-hipster")
 __all__ = [
     "AbsorptionLinePlotter",
+    "CSVGenerator",
     "HiPSGenerator",
     "HTMLGenerator",
     "ImageGenerator",
