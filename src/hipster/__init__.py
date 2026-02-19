ebef888ea91d9495507d017fd84cfd803f3264f6
Bernd Doser <bernd.doser@h-its.org>
2026-02-19 14:19:48 +0100

    Revert "add CSVGenerator"
    This reverts commit f4bfcf9ff570881b9d9c99b02452586e7d58bb7b.

--- src/hipster/__init__.py
+++ src/hipster/__init__.py
@@ -1,7 +1,6 @@
 import importlib.metadata
 
 from .absorption_line_plotter import AbsorptionLinePlotter
-from .csv_generator import CSVGenerator
 from .hips_generator import HiPSGenerator
 from .html_generator import HTMLGenerator
 from .image_generator import ImageGenerator
@@ -15,7 +14,6 @@ from .votable_generator import VOTableGenerator
 __version__ = importlib.metadata.version("astro-hipster")
 __all__ = [
     "AbsorptionLinePlotter",
-    "CSVGenerator",
     "HiPSGenerator",
     "HTMLGenerator",
     "ImageGenerator",
